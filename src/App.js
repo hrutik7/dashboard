bd731cbff0fee87ada144530fc9b646c7b7e98ba
hrutik7 <hrutik.kumthekar19@vit.edu>
2022-04-22 14:01:22 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from "react";
 function App() {
+  const [name, setName] = useState("");
+  const [img, setImg] = useState("");
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    const url = "https://randomuser.me/api"
+
+    const fetchData = async () =>{
+      console.log("in fetch")
+      try{
+        console.log("in try")
+        const response = await fetch(url)
+        const json = await response.json()
+        setName(json.results[0].name.first)
+        setImg(json.results[0].picture.large)
+        setEmail(json.results[0].email)
+        console.log(json.results[0].email)
+      }catch(err){
+        console.log("error" , err)
+      }
+    }
+
+    fetchData();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='name'>
+          <h1>
+          Name :  {
+              name
+            }
+          </h1>
+        <div className='image_dash'>
+              <img alt='' src={img} />
+        </div>
+
+        <div className="email">
+              email : {
+                email
+              }
+        </div>
+      </div>
     </div>
   );
 }
